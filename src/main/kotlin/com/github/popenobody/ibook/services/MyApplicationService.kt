5fc08c57ccf8c9422f67b8c7ba437fbe276e3ff9
GitHub Action <action@github.com>
2022-07-11 15:48:29 +0000

    Template cleanup

--- src/main/kotlin/com/github/popenobody/ibook/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/popenobody/ibook/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.popenobody.ibook.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.popenobody.ibook.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/popenobody/ibook/services/MyProjectService.kt
+++ src/main/kotlin/com/github/popenobody/ibook/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.popenobody.ibook.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.popenobody.ibook.MyBundle
 
 class MyProjectService(project: Project) {
 
